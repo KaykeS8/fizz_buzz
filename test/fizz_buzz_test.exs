bf42afba58a8837b65538956bd9abb382dcdf951
Kayke simao <kayke.py@gmail.com>
2025-04-03 20:28:46 -0300

    Test created

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -3,7 +3,7 @@ defmodule FizzBuzzTest do
 
   describe "build/1" do
     test "when a valid file is provided, returns the converted list" do
-      expected_resonse =  {:ok,[1, 2, :fizz, 4, :buzz, :buzz, :fizz_buzz, :buzz]}
+      expected_resonse = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizz_buzz, :buzz]}
       assert FizzBuzz.build("numbers.txt") == expected_resonse
     end
 
