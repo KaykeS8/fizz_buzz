1480107ceb350196251b3e6f46f886aa5e416e03
Kayke simao <kayke.py@gmail.com>
2025-04-03 20:26:05 -0300

    Format code

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -3,19 +3,20 @@ defmodule FizzBuzz do
 
   defp handle_file({:ok, result}) do
     list =
-    String.split(result, ",")
-    |> Enum.map(&convert_and_evaluate_numbers/1)
+      String.split(result, ",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
     {:ok, list}
   end
 
   defp handle_file({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
   defp convert_and_evaluate_numbers(elem) do
-   String.to_integer(elem)
-   |> evaluate_numbers()
+    String.to_integer(elem)
+    |> evaluate_numbers()
   end
 
-  defp evaluate_numbers(number) when rem(number,3) == 0 and rem(number, 5) == 0, do: :fizz_buzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizz_buzz
   defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
   defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
   defp evaluate_numbers(number), do: number
